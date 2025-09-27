999dacd012ebe56da629c6a792e46e8f8a8a3308
cipriankali <cipriankali@ciprian-kali>
2025-09-27 22:38:11 +0200

    starting to understand react

---
+++ my-app/frontend/src/App.jsx
@@ -0,0 +1,6 @@
+import Name from "./pages/Home.jsx";
+
+function App(){
+    return <div><Name /></div>;
+}
+export default App;
---
+++ my-app/frontend/src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Name(){
+  return <h1>Ciprian</h1>;
+}
+export default Name;
